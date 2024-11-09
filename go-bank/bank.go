48bc8853b3a44f4856c5c224e6d7ed3237046bbe
Suuu-sh <yisshiki39@gmail.com>
2024-11-09 22:19:25 +0900

    fix

--- go-bank/bank.go
+++ go-bank/bank.go
@@ -4,50 +4,51 @@ import "fmt"
 
 func main(){
 	var accountBalance float64 = 1000 
-	fmt.Println("Welcome to Go Bank")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. check balace")
-	fmt.Println("2. deposit money")
-	fmt.Println("3. withdraw money")
-	fmt.Println("4. exit")
-
-	var choice int
-	fmt.Println("Your choice: ")
-	fmt.Scan(&choice)
-
-	fmt.Printf("Your choice %v",choice)
-
-	if choice == 1 {
-      fmt.Println("Your Balance is:", accountBalance)
-	} else if choice == 2 {
-	  fmt.Println("How much do you want to deposit?")
-	  var depositAmount float64
-	  fmt.Scan(&depositAmount)
-
-	  if depositAmount <= 0{
-		fmt.Println("Invalid amount.")
-		return
-	  }
-	  accountBalance += depositAmount
-	  fmt.Println("New accountBalance:",accountBalance)
-	} else if choice == 3 {
-		fmt.Println("How much do you want to withdraw?")
-		var withdrawAmount float64
-		fmt.Scan(&withdrawAmount)
-
-		if withdrawAmount <= accountBalance {
-			fmt.Println("You cant withdraw")
+	for i :=0; i<2; i++ {
+		fmt.Println("Welcome to Go Bank")
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. check balace")
+		fmt.Println("2. deposit money")
+		fmt.Println("3. withdraw money")
+		fmt.Println("4. exit")
+	
+		var choice int
+		fmt.Println("Your choice: ")
+		fmt.Scan(&choice)
+	
+		fmt.Printf("Your choice %v",choice)
+	
+		if choice == 1 {
+		  fmt.Println("Your Balance is:", accountBalance)
+		} else if choice == 2 {
+		  fmt.Println("How much do you want to deposit?")
+		  var depositAmount float64
+		  fmt.Scan(&depositAmount)
+	
+		  if depositAmount <= 0{
+			fmt.Println("Invalid amount.")
 			return
 		  }
-
-		if withdrawAmount > accountBalance {
-			fmt.Println("You cant withdraw")
-			return
-		  }
-		accountBalance -= withdrawAmount
-		fmt.Println("New accountBalance", accountBalance)
-	} else {
-		fmt.Println("Goodbye")
+		  accountBalance += depositAmount
+		  fmt.Println("New accountBalance:",accountBalance)
+		} else if choice == 3 {
+			fmt.Println("How much do you want to withdraw?")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+	
+			if withdrawAmount <= 0 {
+				fmt.Println("You cant withdraw<")
+				return
+			  }
+	
+			if withdrawAmount > accountBalance {
+				fmt.Println("You cant withdraw>")
+				return
+			  }
+			accountBalance -= withdrawAmount
+			fmt.Println("New accountBalance", accountBalance)
+		} else {
+			fmt.Println("Goodbye")
+		}
 	}
-
 }
