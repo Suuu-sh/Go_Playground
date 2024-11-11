7c4e964f13ab9d710ee7592362de1bb2dd34858e
Suuu-sh <yisshiki39@gmail.com>
2024-11-11 23:51:14 +0900

    fix

--- go-basic/profit_calculator.go
+++ go-basic/profit_calculator.go
@@ -1,14 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 func main(){
 	var revenue float64
 	var expenses float64
 	var taxRate float64
 
-	revenue = getUserInput("Revenue: ")
-	expenses = getUserInput("Expenses: ")
-	taxRate = getUserInput("Tax Rate: ")
+	revenue,err1  := getUserInput("Revenue: ")
+
+	expenses,err2 := getUserInput("Expenses: ")
+
+	taxRate,err3  := getUserInput("Tax Rate: ")
+
+	if err1 != nil || err2 != nil || err3 != nil{
+		fmt.Println(err1)
+		return
+	}
 
 	ebt, profit, ratio := calculate_profit(revenue, expenses, taxRate)
 
@@ -17,17 +28,29 @@ func main(){
 	fmt.Println(ratio)
 }
 
-func getUserInput(text string)float64{
+func getUserInput(text string)(float64,error){
 	var userInput float64
 	fmt.Print(text)
 	fmt.Scan(&userInput)
 
-	return userInput
+
+	if userInput <= 0 {
+      return 0, errors.New("value must be apositive")
+	}
+
+	return userInput, nil
 }
-func calculate_profit (revenue, expenses, taxRate float64)(ebt, profit, ratio float64){
-	ebt = revenue - expenses
-	profit = ebt * (1-taxRate/100)
-	ratio = ebt / profit
+func calculate_profit (revenue, expenses, taxRate float64)(float64,float64,float64){
+	ebt := revenue - expenses
+	profit := ebt * (1-taxRate/100)
+	ratio := ebt / profit
+
+	WriteToFile(ebt,profit,ratio)
 
 	return ebt, profit, ratio
+}
+
+func WriteToFile(ebt,profit,ratio float64){
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt,profit,ratio)
+	os.WriteFile("results.txt",[]byte(results),0644)
 }
