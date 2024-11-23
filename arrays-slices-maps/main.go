9be9fa7fbd3ab428b305151ed27b05de9032bef9
Suuu-sh <yisshiki39@gmail.com>
2024-11-23 15:12:27 +0900

    fix

--- arrays-slices-maps/main.go
+++ arrays-slices-maps/main.go
@@ -8,4 +8,22 @@ func main() {
 	userNames = append(userNames, "Max")
 	userNames = append(userNames, "Manuxl")
 
+	courseRatings := make(map[string]float64, 3)
+	courseRatings["go"] = 5.4
+	courseRatings["go3"] = 5.4
+	courseRatings["go4"] = 5.4
+	courseRatings["go5"] = 5.4
+	courseRatings["go53"] = 2.4
+
+	fmt.Println(courseRatings)
+
+	for index, value := range userNames {
+		fmt.Println(index)
+		fmt.Println(value)
+	}
+
+	for index, value := range courseRatings {
+		fmt.Println(index)
+		fmt.Println(value)
+	}
 }
