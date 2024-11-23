2bc220d1c288948d63dc97b05db58cacc3dc1018
Suuu-sh <yisshiki39@gmail.com>
2024-11-23 18:00:01 +0900

    fix

--- function/main.go
+++ function/main.go
@@ -9,12 +9,37 @@ type anotherFn func(int, []string, map[string][]int) ([]int, string)
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{3, 1, 3, 4}
+
 	//copyが生成されないようにpointer &を使用
+	// doubled := transformNumbers(&numbers, double)
+	// tripled := transformNumbers(&numbers, triple)
+
+	// fmt.Println(doubled)
+	// fmt.Println(tripled)
+
+	transformd := transformNumbers(&numbers, func(number int) int {
+		return number * 2
+	})
+	fmt.Println(transformd)
+
+	double := createTransformer(2)
+	triple := createTransformer(3)
+
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 
-	fmt.Println(doubled)
-	fmt.Println(tripled)
+	transformerFn1 := transformFn(&numbers)
+	transformerFn2 := transformFn(&moreNumbers)
+
+}
+
+func transformFn(numbers *[]int) TransformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func transformNumbers(numbers *[]int, transform TransformFn) []int {
@@ -25,10 +50,16 @@ func transformNumbers(numbers *[]int, transform TransformFn) []int {
 	return dNumbers
 }
 
-func double(number int) int {
-	return number * 2
+func createTransformer(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
+	}
 }
 
-func triple(number int) int {
-	return number * 3
-}
+// func double(number int) int {
+// 	return number * 2
+// }
+
+// func triple(number int) int {
+// 	return number * 3
+// }
